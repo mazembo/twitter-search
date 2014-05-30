a2dbd4f7ee11dc66694f65486601100f3e46e4b1
Mazembo <mazemb_eddy@yahoo.fr>
2014-05-30 13:39:12 +0200

    added a scenario test

--- spec/twitter_search_spec.rb
+++ spec/twitter_search_spec.rb
@@ -1,13 +1,20 @@
-require "spec_helper"
+require 'spec_helper'
 
 
-feature "Searching twitter" do
-  scenario "can retrieve results from Twitter" do
-    visit "/"
-    fill_in "search", with: "Johannesburg"
-    click_button "Search"
-    page.should have_content ("Statistics")
+feature 'Searching twitter' do
+  scenario 'can retrieve results from Twitter' do
+    visit '/'
+    fill_in 'search', with: 'Johannesburg'
+    click_button 'Search'
+    page.should have_content ('Statistics')
 
   end
 
+  Scenario 'without a key word' do
+  visit '/'
+  fill_in 'search', with: ''
+  click_button 'Search'
+  page.should have_content ('you did not provide a search term')
+
+end
 end
